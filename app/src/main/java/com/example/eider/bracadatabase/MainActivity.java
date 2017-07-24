53ecbb1cd127a9ab1ae81fd999f4293e169d2e9b
EiderDiaz <eiderdiazmoraila@hotmail.com>
2017-07-24 18:00:43 -0500

    demo final version con indices de resta cuadratica estaticos

--- app/src/main/java/com/example/eider/bracadatabase/MainActivity.java
+++ app/src/main/java/com/example/eider/bracadatabase/MainActivity.java
@@ -62,22 +62,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // you must change this mac addres array with your beacons mac adresses
         String[] MyBeaconsMac = {"F1:82:F5:F1:79:E8", "C0:62:E9:C5:37:F5", "F7:D4:CF:09:98:F0" ,"DD:BE:F9:A1:D8:99","D4:88:E6:45:E1:2B"};
    private final String[][] MatrizRestaCuadratica = new String[][] {
-
-           /*[1]*/      {},
-           /*[2]*/     {},
-           /*[3]*/   {},
-           /*[4]*/    {}
-
+//// TODO: 24/07/2017 agregar matriz de restas cuadratica
+           /*[1]*/      {"4,6.76,0.64 ,1.44 ,5.76","4,12.96,0.36,0.04,17.64","33.64,14.44,0.04,33.64,0.04","17.64,36,14.44,9,0.04"},
+           /*[2]*/     {"2.56,16,2.56,14.44,3.24","16,77.44,31.36,2.56,11.56","12.96,57.76,1.96,0.36,5.76","1.96,7.84,6.76,17.64,0.64"},
+           /*[3]*/   {"2.56,54.76,0.64,3.24,0.16","2.56,0.36,1.96,17.64,9","4,0.64,43.56,0.16,19.36","1,9,2.56,1.96,46.24"},
+           /*[4]*/    {"1.96,10.24,0.16,1.44,14.44","0.64,33.64,0.04,3.24,4.84","6.76,0.04,4,25,21.16","1.96,4.84,36,3.24,10.24"}
 
    };
+    private final String[][] MatrizAtenuaciones = new String[][] {
+//// TODO: 24/07/2017 matriz de atenuaciones
+           /*[1]*/      {"-89,-85.6,-90.8,-90.8,-89.6","-90,-82.4,-93.4,-92.8,-88.8","-91.8,-89.8,-91.2,-88.2,-89.8","-89.8,-89,-92.8,-90,-93.2"},
+           /*[2]*/     {"-92.6,-82,-87.6,-92.8,-88.8","-92,-81.2,-88.4,-91.6,-89.4","-92.6,-90.6,-92.6,-93.4,-90.6","-90.6,-89.2,-89.6,-88.8,-91.8"},
+           /*[3]*/   {"-92.4,-85.4,-90.2,-92.2,-87.6","-92.4,-85.6,-92.6,-89.8,-88","-92,-85.2,-92.6,-90.6,-91.4","-91,-89,-89.4,-92.4,-85.2"},
+           /*[4]*/    {"-92.6,-81.8,-91.4,-92.8,-86.2","-93.8,-81.2,-89.8,-88.2,-89.8","-91.4,-88.2,-87,-92,-89.4","-92.4,-87.8,-85,-91.2,-90.8"}
 
+    };
 
 
     private ListAdapter_BTLE_Devices adapter;
         private ListView listView;
         private TextView err;
 
-        private BroadcastReceiver_BTState mBTStateUpdateReceiver;
+    private BroadcastReceiver_BTState mBTStateUpdateReceiver;
         private Scanner_BTLE mBTLeScanner;
     private Button export;
     private LinearLayout background ;
@@ -301,8 +307,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void stopScan() {
         mBTLeScanner.stop();
 
-
-        //TODO: vaciar el hashmap a un vector 
         if (Buttonflag != 0) {
 
             try {
@@ -328,7 +332,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         ArrayMacAddress[4] = key;
                         ArrayRSSI[4] = String.valueOf(mBTDevicesHashMap.get(key).getRSSI());
                     }
-
                 }
                 for (int i = 0; i < MyBeaconsMac.length; i++) {
                     if (ArrayMacAddress[i].equals(String.valueOf(i + 1))) {
@@ -340,86 +343,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText(this, "error men: " + e.getMessage(), Toast.LENGTH_SHORT).show();
             }
 
-//deaqui
-            AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
-            builder1.setMessage("Write your message here.");
-            builder1.setCancelable(true);
+                    try {
 
-            builder1.setPositiveButton(
-                    "Yes",
-                    new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
                             ArrayList<String> ArraySimilares = new ArrayList<>();
                             int contador = 0;
-                            //   ArraySimilares.add("Resultante:"+Arrays.asList(ArrayRSSI).toString()+"\n"+"similares:");
                             for (int rows = 0; rows < MatrizRestaCuadratica.length; rows++) {
                                 for (int columns = 0; columns < MatrizRestaCuadratica[rows].length; columns++) {
                                     //de la mitad de la matriz en adelante
-                                    String[] StaticVector = VectoresPromediados[rows][columns].split(",");
-                                    //TODO: aqui hago el calculo de la resta cuadratica
-                                    Double ResultadoRestaCuadratica = 0.0;
+                                    String[] StaticVector = MatrizRestaCuadratica[rows][columns].split(",");
+                                    String[] atenuacionVector = MatrizAtenuaciones[rows][columns].split(",");
+
+
+                                    //// TODO: 24/07/2017 aqui se hace la comparacion de vectores de resta cuadratica
+                                    int comparacion = 0;
+                                    String resultadorestacuadratica= "";
+                                    Double res = 0.0;
                                     for (int i = 0; i < 5; i++) {
-                                        Double res = 0.0;
-                                        res = Double.valueOf(StaticVector[i]) - Double.valueOf(ArrayRSSI[i]);
-                                        ResultadoRestaCuadratica += Math.pow(res, 2);
-                                    }
-
-                                    //ArraySimilares.add("R:"+ResultadoRestaCuadratica.toString());
-                                    contador++;
-                                    if (contador == 8) {
-                                        //  ArraySimilares.add("\n");
-                                        contador = 0;
-                                    }
-
-                                    //Toast.makeText(this, "resultado resta cuadratica: "+ ResultadoRestaCuadratica.toString(), Toast.LENGTH_SHORT).show();
-                                    // TODO: 19/07/2017 aqui hacer la comparacion +- 1 contra cada elemento de la matrizRestaCuadratica
-                                    //// TODO: 19/07/2017 se hara una brecha mas grande los datos generados no son parecidos
-                                    String[] splitvector = MatrizRestaCuadratica[rows][columns].split(":");
-                                    Double StaticRestaCuadratica = Double.valueOf(splitvector[1]);
-                                    String coordenada = splitvector[0];
-                                    Double umbralSuperior = ResultadoRestaCuadratica + 2;
-                                    Double umbralInferior = ResultadoRestaCuadratica - 2;
-                                    if (StaticRestaCuadratica <= umbralSuperior && StaticRestaCuadratica >= umbralInferior) {
-                                        ArraySimilares.add("/s/" + coordenada);
+                                        res +=  Math.pow(Double.valueOf(atenuacionVector[i]) - Double.valueOf(ArrayRSSI[i]),2);
+                                        res = Double.valueOf(Math.round(res));
+                                        Double umbralSuperior = res + 2;
+                                        Double umbralInferior = res - 2;
+                                        if (Double.valueOf(StaticVector[i]) <= umbralSuperior &&
+                                                Double.valueOf(StaticVector[i]) >= umbralInferior) {
+                                            comparacion++;
+                                        }
+                                        resultadorestacuadratica+=res+", ";
 
                                     }
+                                 if (comparacion>=2){
+                                     ArraySimilares.add("s:"+Arrays.asList(StaticVector).toString());
+                                 }
 
                                 } // end innner loop
                             } // end upper loop
-                            String similares = "";
-                            try {
-                            for (String elemento : ArraySimilares) {
-                                //// TODO: 21/07/2017 falta guardar en el hashmap los valores de la calibracion para despues compararlos  
-                                if(CalibracionHashMap.containsKey(elemento)){
-                                    Integer.valueOf(CalibracionHashMap.get(elemento));
-                                }
-                                similares += elemento + "\n";
-                            }
-                            err.setText(similares);
+                        String similares = "";
+                        for(String elemento : ArraySimilares){
+                            similares+= elemento +"\n";
                         }
-                            catch (Exception ex){
-                                Toast.makeText(MainActivity.this, ex.getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        
-                        }
-                     
-                    });
+                        err.setText(similares);
 
-            builder1.setNegativeButton(
-                    "No",
-                    new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            dialog.cancel();
-                        }
-                    });
 
-            AlertDialog alert11 = builder1.create();
-            alert11.show();
-
+                    }catch (Exception e){
+                        Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
     }
 
         Buttonflag=0;
-        //err.setText("ya se acabo el pex pushale de nuevo -->");
     }
 
     public void insertInSQLite(String maddress1, String rss1,
